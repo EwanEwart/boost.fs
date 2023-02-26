fd79e06fda358d0f7a38ed56d4b316c1fc5cd1d9
EwanEwart <EwanEwart@HotMail.Co.UK>
2023-02-26 19:58:37 +0100

    using statements

--- boost.fs.01/boost.fs.01.cpp
+++ boost.fs.01/boost.fs.01.cpp
@@ -4,16 +4,20 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
 
+using std::cout;
+using std::endl;
+
 using namespace boost::filesystem;
 
 int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        std::cout << "Usage: tut1 path\n";
+       cout << "Usage: tut1 path\n";
         return 1;
     }
-    std::cout << argv[1] << " " << file_size(argv[1]) << '\n';
+    cout << argv[1] << " " << file_size(argv[1]) << endl;
+
     return 0;
 }
 
